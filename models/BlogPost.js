774801fe98698e22a9dd2e7dbbe14d6271110464
Chrisly <stlegerchrisly18@gmail.com>
2023-11-13 21:11:16 -0500

    modules

---
+++ models/BlogPost.js
@@ -0,0 +1,33 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+class BlogPost extends Model {}
+
+BlogPost.init(
+  {  
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    contents: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'User',
+        key: 'id',
+      },
+      onDelete: 'SET NULL',
+    }
+  },
+  {
+    sequelize,
+    modelName: "BlogPost",
+  }
+);
+
+module.exports = BlogPost;
+
+module.exports = BlogPost;
---
+++ utils/login.js
@@ -0,0 +1,23 @@
+function validatePasswordLength(req, res, next) {
+    const { password } = req.body;
+  
+    if (!password || password.length < 8) {
+      return res.status(400).json({ message: 'Password must be at least 8 characters long' });
+    }
+
+    next();
+  };
+
+  const requireAuth = (req, res, next) => {
+    if (req.session.user_id) {
+        res.locals.loggedIn = true;
+    } else {
+        res.locals.loggedIn = false;
+    }
+    next();
+};
+  
+  module.exports = {
+    validatePasswordLength,
+    requireAuth,
+  }
